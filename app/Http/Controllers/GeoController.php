8b35030fd08b931b092378feb995fb02a58de216
Roman <rubilnik89@mail.ru>
2017-03-31 16:18:56 +0300

    added onclick in search items

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -10,6 +10,25 @@ class GeoController extends Controller
     public function lanLotSearch(Request $request)
     {
         $key = env('GOOGLE_PLACE_API');
+
+        if($request->placeid){
+
+            $ch = curl_init();
+
+            $opt = [CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/details/json?placeid=$request->placeid&key=$key",
+                CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_HEADER => FALSE
+            ];
+            curl_setopt_array($ch, $opt);
+
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($response);
+            Session::forget('items');
+            Session::flash('item', 'Search item');
+            dd($data);
+        }
+
         $lat = $request->latitude;
         $lon = $request->longitude;
         $radius = $request->radius;
@@ -33,25 +52,24 @@ class GeoController extends Controller
 
         $data = json_decode($response);
 
-        if ($request->lanLotSearchPlace) {
-            $place_id = $data->results[1]->place_id;
-
-            $ch = curl_init();
-
-            $opt = [CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/details/json?placeid=$place_id&key=$key",
-                CURLOPT_RETURNTRANSFER => TRUE,
-                CURLOPT_HEADER => FALSE
-            ];
-            curl_setopt_array($ch, $opt);
-
-            $response = curl_exec($ch);
-            curl_close($ch);
-            $data = json_decode($response);
-            Session::forget('items');
-            Session::flash('item', 'Search item');
-            dd($data);
-            return view('lanlotSearch', compact('data', 'photos'));
-        }
+//        if ($request->lanLotSearchPlace) {
+//            $place_id = $data->results[1]->place_id;
+//
+//            $ch = curl_init();
+//
+//            $opt = [CURLOPT_URL => "https://maps.googleapis.com/maps/api/place/details/json?placeid=$place_id&key=$key",
+//                CURLOPT_RETURNTRANSFER => TRUE,
+//                CURLOPT_HEADER => FALSE
+//            ];
+//            curl_setopt_array($ch, $opt);
+//
+//            $response = curl_exec($ch);
+//            curl_close($ch);
+//            $data = json_decode($response);
+//            Session::forget('items');
+//            Session::flash('item', 'Search item');
+//            dd($data);
+//        }
         $photos = [];
         for ($i = 0; $i < count($data->results); $i++) {
             $photoreference = isset($data->results[$i]->photos) ? $data->results[$i]->photos[0]->photo_reference : 'nophoto';
--- resources/views/lanlotSearch.blade.php
+++ resources/views/lanlotSearch.blade.php
@@ -33,8 +33,8 @@
                                         </thead>
                                         <tbody>
                                         @foreach($data->results as $index => $result)
-                                            <tr>
-                                            {{--<tr class="clickable" onclick="window.location.href='{{ route('user', ['id' => $account->user->id]) }}';">--}}
+                                            {{--<tr>--}}
+                                            <tr class="clickable" onclick="window.location.href='{{ route('lanLotSearch', ['placeid' => $data->results[$index]->place_id]) }}';">
                                                 <td class="col-md-3"><img src="{{ $result->icon }}"></td>
                                                 <td class="col-md-3">{{ $result->name }}</td>
                                                 @if($photos[$index] == 'nophoto')
@@ -61,49 +61,49 @@
                         </div>
                     @endif
 
-                    @if(Session::has('item'))
-                        <div class="col-md-8">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">Результаты поиска</div>
-                                <div class="panel-body">
-                                    <table class="table table-hover table-striped">
-                                        <thead>
-                                        <tr>
-                                            <th class="col-md-1">Icon</th>
-                                            <th class="col-md-1">Name</th>
-                                            <th class="col-md-1">Photo</th>
-                                            <th class="col-md-1">Type</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($data->results as $index => $result)
-                                            <tr>
-                                                {{--<tr class="clickable" onclick="window.location.href='{{ route('user', ['id' => $account->user->id]) }}';">--}}
-                                                <td class="col-md-3"><img src="{{ $result->icon }}"></td>
-                                                <td class="col-md-3">{{ $result->name }}</td>
-                                                @if($photos[$index] == 'nophoto')
-                                                    <td class="col-md-3">No photo</td>
-                                                @else
-                                                    <td class="col-md-3"><img src="{{ $photos[$index] }}"></td>
-                                                @endif
-                                                <td class="col-md-3">
-                                                    @foreach($result->types as $type)
-                                                        {!! $type .'<br>' !!}
-                                                    @endforeach
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                                @if(isset($data->next_page_token))
-                                    <div class="pull-right">
-                                        <a class="btn btn-default" href="{{ route('lanLotSearch', ['next' => $data->next_page_token]) }}" role="button">Следующие 20</a>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                    @endif
+                    {{--@if(Session::has('item'))--}}
+                        {{--<div class="col-md-8">--}}
+                            {{--<div class="panel panel-default">--}}
+                                {{--<div class="panel-heading">Результаты поиска</div>--}}
+                                {{--<div class="panel-body">--}}
+                                    {{--<table class="table table-hover table-striped">--}}
+                                        {{--<thead>--}}
+                                        {{--<tr>--}}
+                                            {{--<th class="col-md-1">Icon</th>--}}
+                                            {{--<th class="col-md-1">Name</th>--}}
+                                            {{--<th class="col-md-1">Photo</th>--}}
+                                            {{--<th class="col-md-1">Type</th>--}}
+                                        {{--</tr>--}}
+                                        {{--</thead>--}}
+                                        {{--<tbody>--}}
+                                        {{--@foreach($data->results as $index => $result)--}}
+                                            {{--<tr>--}}
+                                                {{--<tr class="clickable" onclick="window.location.href='{{ route('lanLotSearch', ['placeIid' => $data->results[$index]->place_id]) }}';">--}}
+                                                {{--<td class="col-md-3"><img src="{{ $result->icon }}"></td>--}}
+                                                {{--<td class="col-md-3">{{ $result->name }}</td>--}}
+                                                {{--@if($photos[$index] == 'nophoto')--}}
+                                                    {{--<td class="col-md-3">No photo</td>--}}
+                                                {{--@else--}}
+                                                    {{--<td class="col-md-3"><img src="{{ $photos[$index] }}"></td>--}}
+                                                {{--@endif--}}
+                                                {{--<td class="col-md-3">--}}
+                                                    {{--@foreach($result->types as $type)--}}
+                                                        {{--{!! $type .'<br>' !!}--}}
+                                                    {{--@endforeach--}}
+                                                {{--</td>--}}
+                                            {{--</tr>--}}
+                                        {{--@endforeach--}}
+                                        {{--</tbody>--}}
+                                    {{--</table>--}}
+                                {{--</div>--}}
+                                {{--@if(isset($data->next_page_token))--}}
+                                    {{--<div class="pull-right">--}}
+                                        {{--<a class="btn btn-default" href="{{ route('lanLotSearch', ['next' => $data->next_page_token]) }}" role="button">Следующие 20</a>--}}
+                                    {{--</div>--}}
+                                {{--@endif--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
 
                     <div class="col-md-2">
                         <div class="panel panel-default">
@@ -123,23 +123,23 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Поиск (первого)места по координатам</div>
-                            <div class="panel-body">
-                                {{ Form::open(array('action' => array('GeoController@lanLotSearch'), 'method' => 'get')) }}
-                                <input name="lanLotSearchPlace" type="hidden" value="1">
-                                <label for="latitude">Search by account latitude</label>
-                                <input id="latitude" class="form-control" name="latitude" placeholder="latitude">
-                                <label for="longitude">Search by account longitude</label>
-                                <input id="longitude" class="form-control" name="longitude" placeholder="longitude">
-                                <label for="radius">Radius</label>
-                                <input id="radius" class="form-control" name="radius" placeholder="radius (m)">
-                                <button class="btn btn-primary" type="submit">OK</button>
-                                {{ Form::close() }}
-                            </div>
-                        </div>
-                    </div>
+                    {{--<div class="col-md-2">--}}
+                        {{--<div class="panel panel-default">--}}
+                            {{--<div class="panel-heading">Поиск (первого)места по координатам</div>--}}
+                            {{--<div class="panel-body">--}}
+                                {{--{{ Form::open(array('action' => array('GeoController@lanLotSearch'), 'method' => 'get')) }}--}}
+                                {{--<input name="lanLotSearchPlace" type="hidden" value="1">--}}
+                                {{--<label for="latitude">Search by account latitude</label>--}}
+                                {{--<input id="latitude" class="form-control" name="latitude" placeholder="latitude">--}}
+                                {{--<label for="longitude">Search by account longitude</label>--}}
+                                {{--<input id="longitude" class="form-control" name="longitude" placeholder="longitude">--}}
+                                {{--<label for="radius">Radius</label>--}}
+                                {{--<input id="radius" class="form-control" name="radius" placeholder="radius (m)">--}}
+                                {{--<button class="btn btn-primary" type="submit">OK</button>--}}
+                                {{--{{ Form::close() }}--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
                 </div>
             </div>
